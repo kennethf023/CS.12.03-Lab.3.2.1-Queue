34fd8d2aa8fb5eeb6dd363bfd8b1baa1cb61c96a
kennethfu <kennethfu023@gmail.com>
2024-03-07 08:59:00 +0800

    commit

--- src/main/java/BreadthFirstSearch.java
+++ src/main/java/BreadthFirstSearch.java
@@ -8,36 +8,62 @@ public class BreadthFirstSearch {
      */
     public static int[] bfs(int[][] graph) {
 
+        if (graph == null || graph.length == 0) {
+            return new int[0]; // Empty graph, return an empty array
+        }
+
+        int numNodes = graph.length;
+
         // Create a Queue and add the first node (0) to it. Use your Queue class that you just created!
+        CISQueue<Integer> queue = new CISQueue<Integer>(0);
 
         // Create a visited array. This array will track whether we have visited a specific node.
+        boolean[] visited = new boolean[numNodes];
 
         // Create a bfs path array and a bfs path index. This array will track the order that the nodes were visited.
+        int[] bfsPath = new int[numNodes];
+        int bfsPathIndex = 0;
 
-        // Add the first node (0) to the queue.
+        // Add the first node (0) to the queue. --> done in constructor
 
         // Record the first node (0) as visited.
-        
+        visited[0] = true;
+
+
         // Time to traverse the graph! While the queue is not empty ...
 
-            // Dequeue (poll) the queue and store this value in a variable called currentNode.
+        // Dequeue (poll) the queue and store this value in a variable called currentNode.
 
-            // Record this node as visited.
+        // Record this node as visited.
 
-            // Add this node to the path and update the path index.
+        // Add this node to the path and update the path index.
 
-            // Obtain an array of this node's neighbouring/adjacent nodes.
-        
-            // Explore the current nodes neighbouring nodes. For each neighbouring node ...
+        // Obtain an array of this node's neighbouring/adjacent nodes.
 
-                // If a neighbour hasn't been visited before ...
+        // Explore the current nodes neighbouring nodes. For each neighbouring node ...
 
-                    // Record the neighbour as visited.
-        
-                    // Add the neighbour to the queue. 
+        // If a neighbour hasn't been visited before ...
+
+        // Record the neighbour as visited.
+
+        // Add the neighbour to the queue.
+
+        while (!queue.isEmpty()) {
+            int currentNode = queue.dequeue();
+
+            bfsPath[bfsPathIndex++] = currentNode;
+
+            int[] neighbors = graph[currentNode];
+
+            for (int neighbor : neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.enqueue(neighbor);
+                }
+            }
+        }
 
         // Return bfs path.
-        
-        return null;
+        return bfsPath;
     }
 }
